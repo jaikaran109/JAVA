cbcc8c006f88bdebda035622208a870e93f88eb6
jaikaran gupta <jaik05581@gmail.com>
2025-08-24 16:41:26 +0530

    Q1.java

---
+++ BASIC/PATTERN/Q1.java
@@ -0,0 +1,12 @@
+// Java Program to Read The Number From Standard Input
+
+
+import java.util.Scanner;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Number :");
+        int n = input.nextInt();
+        System.out.println("Your Entered Number is " + n);
+    }
+}
