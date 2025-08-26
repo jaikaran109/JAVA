21ad795c55c6f8b26778707280eba6ee3115088d
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:32:50 +0530

    Create Q17.java

---
+++ Basics/Q17.java
@@ -0,0 +1,20 @@
+// Write a program to find the sum of n natural numbers
+
+
+
+import java.util.Scanner;
+public class Q17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if(n>0){
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println("Sum of All Natural Number b/w 1 to N is :"+sum);
+    }else{
+        System.out.println("Number is Invalid");
+    }
+    }
+}
