437505b065a6fcd1f99b25efc5456ae6e1ff9c36
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:32:07 +0530

    Create Q15.java

---
+++ Basics/Q15.java
@@ -0,0 +1,23 @@
+// Java Program to Check whether the input number is a Neon Number.
+
+
+
+import java.util.Scanner;
+public class Q15 {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int a = input.nextInt();
+    int b = a*a;
+    int res = 0;
+
+    while(b!=0){
+        res=b%10+res;
+        b=b/10;
+    }
+    if(res == a){
+        System.out.println("Number is Neon Number");
+    }else{
+        System.out.println("its Not");
+    }
+}
+}
