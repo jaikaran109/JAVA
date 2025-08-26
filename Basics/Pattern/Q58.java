092d7cd219a3c83db651cc182a4ee9df4fb4011e
jaikaran109 <jaik05581@gmail.com>
2025-08-26 17:49:17 +0530

    Create Q58.java

---
+++ Basics/Pattern/Q58.java
@@ -0,0 +1,31 @@
+// Q:-58 Print the following Pattern.
+
+
+// 1        1
+// 12      21
+// 123    321
+// 1234  4321
+// 1234554321
+
+
+
+import java.util.Scanner;
+public class Q58 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int k = 1;k <= 2*(n-i);k++){
+                System.out.print("  ");
+            }
+            for(int k=i;k>0;k--){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
