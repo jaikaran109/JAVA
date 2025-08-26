da8d1599cba48b65e04a18f5d06206028c434fdb
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:34:57 +0530

    Create Q30.java

---
+++ Basics/Q30.java
@@ -0,0 +1,16 @@
+// Java Program to Check if a Number is Positive or Negative.
+import java.util.Scanner;
+public class Q30 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if(n<0){
+            System.out.println("Input is negstive");
+        }else if(n==0){
+            System.out.println("Input is zero");
+        }else{
+            System.out.println("Input is Positive");
+        }
+        }
+    }
+
