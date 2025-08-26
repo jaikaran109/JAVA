0f97eb96a58c8ba400ca1798efddb87dd3350b9a
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:27:07 +0530

    Create Q3.java

---
+++ Basics/Q3.java
@@ -0,0 +1,12 @@
+// Java Program to Multiply Two Floating-Point Numbers
+
+
+import java.util.Scanner;
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        System.out.println("The Answer is :" + a*b);
+    }
+}
