2f45f80451aa45d8633a005f21c28a74af6b536d
jaikaran109 <jaik05581@gmail.com>
2025-08-26 22:17:39 +0530

    Create Q5.java

---
+++ Basics/Q5.java
@@ -0,0 +1,29 @@
+// Java Program to Add Two Binary Strings
+
+// take a look *
+
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first binary string: ");
+        String bin1 = input.nextLine();
+
+        System.out.print("Enter second binary string: ");
+        String bin2 = input.nextLine();
+
+
+        int num1 = Integer.parseInt(bin1, 2);
+        int num2 = Integer.parseInt(bin2, 2);
+
+
+        int sum = num1 + num2;
+
+        String result = Integer.toBinaryString(sum);
+
+        System.out.println("Sum of binary strings: " + result);
+    }
+}
