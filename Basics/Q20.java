e1d540460a39bd1e0408b3f1ca3ac16e3a96e808
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:33:55 +0530

    Create Q20.java

---
+++ Basics/Q20.java
@@ -0,0 +1,14 @@
+//calculate SI
+
+
+import java.util.Scanner;
+public class Q20 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        Double principle=input.nextDouble();
+        Double rate=input.nextDouble();
+        Double time=input.nextDouble();
+        Double SI=(principle*rate*time)/100;
+        System.out.println("Simple Interest is :"+SI);
+    }
+}
