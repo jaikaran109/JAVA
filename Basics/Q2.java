261d85482bcc802910c9556c6a04e6b339e96223
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:26:37 +0530

    Create Q2.java

---
+++ Basics/Q2.java
@@ -0,0 +1,14 @@
+// Java Program to Get Input from the User 
+
+
+import java.util.Scanner;
+
+class Q2{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter UR number");
+        int  a = input.nextInt();
+        System.out.println("The input is :" + a);
+        
+    }
+}
