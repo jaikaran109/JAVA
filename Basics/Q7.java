e22531721ea6824cafe0b97b03ae53178c7505a8
jaikaran gupta <jaik05581@gmail.com>
2025-08-26 16:27:59 +0530

    Create Q7.java

---
+++ Basics/Q7.java
@@ -0,0 +1,16 @@
+// Java Program to Check Even or Odd Integers
+
+
+
+import java.util.Scanner;
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        if(a % 2 == 0){
+            System.out.println("Even");
+        } else{
+            
+        }
+    }
+}
