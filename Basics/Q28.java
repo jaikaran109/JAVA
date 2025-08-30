b85dda3a7b417361293d1de6ba8655dd584be686
jaikaran109 <jaik05581@gmail.com>
2025-08-30 23:09:27 +0530

    Create Q28.java

---
+++ Basics/Q28.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Q28 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char a = input.next().charAt(0);
+        int ascii = a;
+        System.out.println("The ASCII Value of a is : " + ascii);
+    }
+}
