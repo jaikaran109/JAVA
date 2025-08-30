c8160e700dabfdba19325c63d5f22c8ec908f45a
jaikaran109 <jaik05581@gmail.com>
2025-08-30 23:04:05 +0530

    Create Q27.java

---
+++ Basics/Q27.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int rem=0;
+        int rev = 0;
+        while(num!=0){
+            rem = num % 10;
+            rev = rev*10 + rem;
+            num /= 10;
+        }
+        System.out.println(rev);
+    }
+}
