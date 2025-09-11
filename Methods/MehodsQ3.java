b3a8fe18c3521092d4e60ff2d18cce7ed9f4da37
jaikaran109 <jaik05581@gmail.com>
2025-09-11 16:07:26 +0530

    Create MehodsQ3.java

---
+++ Methods/MehodsQ3.java
@@ -0,0 +1,19 @@
+//Swap Numbers
+
+import java.util.Scanner;
+public class MethodsQ3 {
+
+    public static void swap(int a,int b){
+        a = a + b ;
+        b = a - b ;
+        a = a - b;
+        System.out.println(a+" "+b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+" "+b); 
+        swap(a,b);
+    }
+}
