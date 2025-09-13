0d0a13edfeb534a5eec0d187d5ad95a80a7b376c
jaikaran109 <jaik05581@gmail.com>
2025-09-13 21:25:27 +0530

    Create Subtring.java

---
+++ String/Subtring.java
@@ -0,0 +1,8 @@
+public class substring {
+    public static void main(String[] args) {
+        String name="jaikaran";
+        System.out.println(name.substring(0,4));
+        System.out.println(name.substring(4,8));
+        System.out.println(name.length());
+    }
+}
