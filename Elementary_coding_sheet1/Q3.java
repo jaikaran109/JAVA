c1ddf6833fa75137417307bb49f4a44ead8fb920
jaikaran109 <jaik05581@gmail.com>
2025-11-13 02:07:21 +0530

    Create Q3.java

---
+++ Elementary_coding_sheet1/Q3.java
@@ -0,0 +1,27 @@
+package Elementary_Coding1;
+
+
+//Return last digit of the given number.
+//
+//Write a function that returns the last digit of the given number. Last digit is being referred to the least significant digit i.e. the digit in the ones (units) place in the given number.
+//
+//The last digit should be returned as a positive number. for example,
+//
+//if the given number is 197, the last digit is 7 if the given number is -197, the last digit is 7
+
+
+import java.util.*;
+public class Q3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		if(num < 0) {
+			num *= (-1);
+		}
+		num /= 10;
+		System.out.print(num % 10);
+
+	}
+
+}
