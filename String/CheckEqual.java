4d5880e8ee01449cf46187990ce4108076505337
jaikaran109 <jaik05581@gmail.com>
2025-11-22 10:16:28 +0530

    Create CheckEqual.java

---
+++ String/CheckEqual.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.*;
+
+public class checkEqual {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.next();
+		String gtr = input.next();
+		System.out.print(str.equals(gtr));
+	}
+
+}
