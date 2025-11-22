5adebc730fea5455aa5cfd5cc6a5a6997f6ab735
jaikaran109 <jaik05581@gmail.com>
2025-11-22 10:15:58 +0530

    Create ASCII.java

---
+++ String/ASCII.java
@@ -0,0 +1,16 @@
+package String;
+
+import java.util.*;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		char ch = input.next().charAt(0);
+		
+		int ascii = (int) ch; 
+		
+		 System.out.println(ascii);
+	}
+
+}
