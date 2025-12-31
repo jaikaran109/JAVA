e2d4ae285c0c7036b828f218f27ad9e36117b53f
jaikaran109 <jaik05581@gmail.com>
2026-01-01 02:02:38 +0530

    Update and rename Q19.java to Q19EvenSumFibonacci.java

--- Basics/Q19EvenSumFibonacci.java
+++ Basics/Q19EvenSumFibonacci.java
@@ -5,20 +5,24 @@
 
 
 import java.util.Scanner;
+
 public class Q19 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter N");
         int n = input.nextInt();
-        int a = 0;
-        int b = 1;
+
+        int a = 0, b = 1;
         int sum = 0;
-        for(int i = 1 ;i<=n;i++){
-            sum+=a;
-            int next = a+b;
-            a=b;
-            b=next;
+
+        for(int i = 1; i <= n; i++){
+            if(a % 2 == 0){
+                sum += a;
+            }
+            int next = a + b;
+            a = b;
+            b = next;
         }
-        System.out.println("Sum till N"+sum);
+
+        System.out.println(sum);
     }
 }
