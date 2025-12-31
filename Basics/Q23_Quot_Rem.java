506c9565a8e7e6d1b8c7319a00f9804c8409b91a
jaikaran109 <jaik05581@gmail.com>
2026-01-01 02:11:24 +0530

    Rename Q23.java to Q23_Quot_Rem.java

--- Basics/Q23_Quot_Rem.java
+++ Basics/Q23_Quot_Rem.java
@@ -3,28 +3,24 @@
 
 
 import java.util.Scanner;
+
 public class Q23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-
-        System.out.print("Enter the dividend: ");
         int dividend = input.nextInt();
-
-
-        System.out.print("Enter the divisor: ");
         int divisor = input.nextInt();
-        
-        if (divisor == 0) {
-            System.out.println("Error: Division by zero is not allowed.");
-        }
 
+        if (divisor == 0) {
+            System.out.println("Division by zero not allowed");
+            return;
+        }
 
         int quotient = dividend / divisor;
         int remainder = dividend % divisor;
 
-        System.out.println("Quotient is: " + quotient);
-        System.out.println("Remainder is: " + remainder);
-
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
     }
 }
+
