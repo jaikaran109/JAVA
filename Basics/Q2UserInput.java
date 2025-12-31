e44e141161eac8e03c1c13f3762fdc1e034bffc1
jaikaran109 <jaik05581@gmail.com>
2026-01-01 02:04:03 +0530

    Add Q2UserInput.java file

--- Basics/Q2UserInput.java
+++ Basics/Q2UserInput.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 class Q2{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter UR number");
         int  a = input.nextInt();
-        System.out.println("The input is :" + a);
+        System.out.println(a);
         
     }
 }
