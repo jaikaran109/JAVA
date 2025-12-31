0d5713146672196c6e8773239d37238f7e633cc5
jaikaran109 <jaik05581@gmail.com>
2026-01-01 01:43:34 +0530

    Update Q10_GCD.java

--- Basics/Q10_GCD.java
+++ Basics/Q10_GCD.java
@@ -2,25 +2,21 @@
 
 
 import java.util.Scanner;
+
 public class Q10 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int a = input.nextInt();
         int b = input.nextInt();
-        int least = least(a,b);
-        int i = 2;
-        while(i<=least){
-            if(a%i==0 && b%i==0){
-                System.out.println(i);
-            }
-            i++;
+
+        while(b != 0){
+            int rem = a % b;
+            a = b;
+            b = rem;
         }
+
+        System.out.println("GCD = " + a);
     }
-    public static int least(int a,int b) {
-        if(a<b){
-            return a;
-        }else{
-            return b;
-        }
-    }
+}
+
 }
