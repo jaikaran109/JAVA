1f45536afc1f8b02633d1bc7f7a5f011e3886f6d
jaikaran109 <jaik05581@gmail.com>
2026-01-01 01:58:01 +0530

    Add Q17_Sum_Natural_Num.java file

--- Basics/Q17_Sum_Natural_Num.java
+++ Basics/Q17_Sum_Natural_Num.java
@@ -8,13 +8,13 @@ public class Q17 {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         if(n>0){
-        int sum = 0;
-        for(int i=1;i<=n;i++){
-            sum+=i;
-        }
-        System.out.println("Sum of All Natural Number b/w 1 to N is :"+sum);
-    }else{
-        System.out.println("Number is Invalid");
+            int sum = 0;
+            for(int i=1;i<=n;i++){
+                sum+=i;
+            }
+            System.out.println("Sum of All Natural Number b/w 1 to N is :"+sum);
+        }else{
+            System.out.println("Number is Invalid");
     }
     }
 }
