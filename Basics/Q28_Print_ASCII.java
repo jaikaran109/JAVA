b6f3d44168feb1df2d2cd8f88bc763abd6d8e9df
jaikaran109 <jaik05581@gmail.com>
2026-01-01 02:20:44 +0530

    Update and rename Q28.java to Q28_Print_ASCII.java

--- Basics/Q28_Print_ASCII.java
+++ Basics/Q28_Print_ASCII.java
@@ -1,9 +1,16 @@
+//How to Print ASCII Value in Java
+
+
+
 import java.util.Scanner;
+
 public class Q28 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        char a = input.next().charAt(0);
-        int ascii = a;
-        System.out.println("The ASCII Value of a is : " + ascii);
+
+        char ch = input.next().charAt(0);
+        int ascii = ch;
+
+        System.out.println(ascii);
     }
 }
