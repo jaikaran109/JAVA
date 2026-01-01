0463b309106e92d9482550acd6b814a42eaa10f4
jaikaran109 <jaik05581@gmail.com>
2026-01-02 01:25:03 +0530

    Add Q57_Triangle_0_1.java for triangle pattern

---
+++ Basics/Pattern/Q57_Triangle_0_1.java
@@ -0,0 +1,30 @@
+package Pattern;
+
+
+//1 
+//0 1 
+//1 0 1 
+//0 1 0 1 
+//1 0 1 0 1 
+
+
+import java.util.Scanner;
+
+public class Triangle0_1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		for(int i = 1 ; i <= n ; i++) {
+			for(int j = 1 ; j <= i ; j++) {
+				if((j+i) % 2 == 0)
+					System.out.print("1 ");
+				else
+					System.out.print("0 ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
