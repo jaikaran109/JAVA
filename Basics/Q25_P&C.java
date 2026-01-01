128ab064307ad6a8939b618ecd3118fe2914503c
jaikaran109 <jaik05581@gmail.com>
2026-01-01 23:49:07 +0530

    Update and rename Q25.java to Q25_P&C.java

--- Basics/Q25_P&C.java
+++ Basics/Q25_P&C.java
@@ -1,9 +1,13 @@
 // Permutation and Combination Program
 
+
+
+
+
 import java.util.Scanner;
+
 public class Q25 {
 
- // Method to calculate factorial
     static int factorial(int n) {
         int fact = 1;
         for (int i = 2; i <= n; i++) {
@@ -11,27 +15,26 @@ public class Q25 {
         }
         return fact;
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter value of n ");
-        int n = input.nextInt();
 
-        System.out.println("Enter value of r ");
+        int n = input.nextInt();
         int r = input.nextInt();
 
-        if(r > n){
-            System.out.println("Invalid Input : r not greater than n. ");
+        if (r > n || n < 0 || r < 0) {
+            System.out.println("Invalid Input");
+            return;
         }
 
         int permutation = factorial(n) / factorial(n - r);
-
         int combination = factorial(n) / (factorial(r) * factorial(n - r));
 
         System.out.println("Permutation (nPr): " + permutation);
         System.out.println("Combination (nCr): " + combination);
-
     }
 }
+
 
 
 // Permutation: nPr = n! / (n - r)!
