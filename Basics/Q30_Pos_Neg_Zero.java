7a0811a340b7c1cbb8d5621022191a20eb25b3f0
jaikaran109 <jaik05581@gmail.com>
2026-01-01 13:02:42 +0530

    Update and rename Q30.java to Q30_Pos_Neg_Zero.java

--- Basics/Q30_Pos_Neg_Zero.java
+++ Basics/Q30_Pos_Neg_Zero.java
@@ -1,12 +1,17 @@
 // Java Program to Check if a Number is Positive or Negative.
+
+
+
 import java.util.Scanner;
 public class Q30 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         int n = input.nextInt();
-        if(n<0){
-            System.out.println("Input is negstive");
-        }else if(n==0){
+        
+        if(n < 0){
+            System.out.println("Input is negative");
+        }else if(n == 0){
             System.out.println("Input is zero");
         }else{
             System.out.println("Input is Positive");
