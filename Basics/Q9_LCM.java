551293ecc99d572a303cbd9be358f191474cac8b
jaikaran109 <jaik05581@gmail.com>
2026-01-01 23:46:11 +0530

    Update and rename Q9.java to Q9_LCM.java

--- Basics/Q9_LCM.java
+++ Basics/Q9_LCM.java
@@ -2,30 +2,36 @@
 
 
 
+
 import java.util.Scanner;
+
 public class Q9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         int a = input.nextInt();
         int b = input.nextInt();
-        int least = least(a,b);
+
+        int x = a, y = b;
         int LCM = 1;
         int i = 2;
-        while(i<=least){
-            if(a%i==0 && b%i==0){
-                LCM *=i;
+        int least = least(a, b);
+
+        while (i <= least) {
+            if (x % i == 0 && y % i == 0) {
+                LCM *= i;
+                x /= i;
+                y /= i;
             } else {
-                LCM = a*b;
+                i++;
             }
-            i++;
         }
+
+        LCM = LCM * x * y;
         System.out.println(LCM);
     }
-    public static int least(int a,int b) {
-        if(a<b){
-            return a;
-        }else{
-            return b;
-        }
+
+    public static int least(int a, int b) {
+        return (a < b) ? a : b;
     }
 }
