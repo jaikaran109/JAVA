98f5b3705a6be1c0c3575c89618f9fdf0236ebb8
jaikaran109 <jaik05581@gmail.com>
2026-01-02 01:27:30 +0530

    Update and rename Q58.java to Q58_Num_Palindrome.java

--- Basics/Pattern/Q58_Num_Palindrome.java
+++ Basics/Pattern/Q58_Num_Palindrome.java
@@ -16,13 +16,13 @@ public class Q58 {
         int n = input.nextInt();
         for(int i =1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+                System.out.print(j);
             }
             for(int k = 1;k <= 2*(n-i);k++){
-                System.out.print("  ");
+                System.out.print(" ");
             }
             for(int k=i;k>0;k--){
-                System.out.print(k+" ");
+                System.out.print(k);
             }
             System.out.println();
         }
