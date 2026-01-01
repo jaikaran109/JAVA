8fdd44d45f4548bf933e0085a292da7f0d0098f0
jaikaran109 <jaik05581@gmail.com>
2026-01-02 01:21:41 +0530

    Update and rename Q55.java to Q55_Inverted_triangle.java

--- Basics/Pattern/Q55_Inverted_triangle.java
+++ Basics/Pattern/Q55_Inverted_triangle.java
@@ -7,19 +7,23 @@
 //     *
 
 
-
 import java.util.Scanner;
+
 public class Q55 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        for (int i =1;i<=n;i++){
-            for(int  j = 1;j<=i;j++){
-                System.out.print("  ");
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
             }
-            for(int k = n;k>=2*i;k--){
-                System.out.print("* ");
+
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
             }
+
             System.out.println();
         }
     }
