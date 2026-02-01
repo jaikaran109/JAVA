8e0845ac82888d82ce00e1f5e38ef1d0867e4ec0
jaikaran109 <jaik05581@gmail.com>
2026-02-02 01:00:50 +0530

    Update ElementFrequency.java

--- Assessment–01/ElementFrequency.java
+++ Assessment–01/ElementFrequency.java
@@ -1,28 +1,28 @@
 package Assisment1;
 import java.util.*;
-public class ElementFrequency {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class Frequency {
+    public static void frequency(int[] nums) {
+        boolean[] visit = new boolean[nums.length];
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(visit[i]) continue;   // jo count ho chuka h usko skip kr dega
 
-        int size = input.nextInt();
-        int[] arr = new int[size];
-        for(int i = 0 ; i < size ; i++){
-            arr[i] = input.nextInt();
-        }
-
-        boolean[] visited = new boolean[size];
-        for(int i = 0 ; i < size ; i++){
-            if(visited[i]) continue;
-
-            int count = 1;
-
-            for(int j = i + 1 ; j < size ; j++){
-                if(arr[i] == arr[j]){
-                    count++;
-                    visited[j] = true;
+            int freq = 1;
+            for(int j = i + 1 ; j < nums.length ; j++) {
+                if(nums[i] == nums[j]) {
+                    freq++;
+                    visit[j] = true;   // jis position ki value count hogi usko true mark kr dega
                 }
             }
-            System.out.println(arr[i] +" --> " + count);
+            System.out.println(nums[i] + " - > "+ freq);
         }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0 ;i < size ;i++){
+            arr[i] = input.nextInt();
+        }
+        frequency(arr);
     }
 }
