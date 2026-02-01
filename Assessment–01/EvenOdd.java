0b1a4612731c13be0e61dea4fb1f1339066e72ed
jaikaran109 <jaik05581@gmail.com>
2026-02-02 00:59:24 +0530

    Create EvenOdd.java

---
+++ Assessment–01/EvenOdd.java
@@ -0,0 +1,13 @@
+package Assisment1;
+
+//  --  Even / Odd number
+
+import java.util.*;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if(n % 2 == 0) System.out.println("Even");
+        else System.out.println("Odd");
+    }
+}
