878cd72295f175f2653180e2f347655342ee7494
jaikaran109 <jaik05581@gmail.com>
2026-05-17 19:30:03 +0530

    Update PalindromeLinkedList.java

--- sem4_EndTerm/LinkedList/PalindromeLinkedList.java
+++ sem4_EndTerm/LinkedList/PalindromeLinkedList.java
@@ -14,3 +14,24 @@ class Solution {
         return left >= right;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         List<Integer> list = new ArrayList<>();
+//         while(head != null){
+//             list.add(head.val);
+//             head = head.next;
+//         }
+//         int l = 0;
+//         int r = list.size() - 1;
+//         while(l < r){
+//             if(list.get(l) != list.get(r)) return false;
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+// }
